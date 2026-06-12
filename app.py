c6358835e7520d4dd1182c83c384f5cd1480c7fa
saipranitha-123 <saipranithagone4@gmail.com>
2026-06-12 21:06:39 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,27 @@
+import streamlit as st
+from deep_translator import GoogleTranslator
+
+st.title("🌍 Language Translation Tool")
+
+text = st.text_area("Enter Text")
+
+languages = {
+    "English": "en",
+    "Hindi": "hi",
+    "Telugu": "te",
+    "French": "fr",
+    "Spanish": "es"
+}
+
+source = st.selectbox("Source Language", list(languages.keys()))
+target = st.selectbox("Target Language", list(languages.keys()))
+
+if st.button("Translate"):
+    translated = GoogleTranslator(
+        source=languages[source],
+        target=languages[target]
+    ).translate(text)
+
+    st.success("Translation Completed!")
+    st.write("Translated Text:")
+    st.write(translated)
